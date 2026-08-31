d779f5ac78b44494c6e05e56e96b6a30b0e298c0
d-upre <140529270+d-upre@users.noreply.github.com>
2026-08-31 19:29:30 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,8 @@
+local Games = {
+    10851599 = "rts"
+}
 
+local Found = Games[game.PlaceId]
+if Found then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/d-upre/rks-hub/refs/heads/main/games/"..Found..".lua"))()
+end
