becd32494cbbf9be3b44c30377e4ee572d67fde7
d-upre <140529270+d-upre@users.noreply.github.com>
2026-08-31 20:19:56 -0400

    Update mr.lua

--- games/mr.lua
+++ games/mr.lua
@@ -3,30 +3,32 @@
 -----------------------------
 
 local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 -----------------------------
 -- Variables & Functions
 -----------------------------
 
 local LocalPlayer = Players.LocalPlayer
+local PlayerScripts = LocalPlayer:FindFirstChildOfClass("PlayerScripts")
 
 -----------------------------
 -- Interface
 -----------------------------
 
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/d-upre/rks-hub/refs/heads/main/assets/ui.lua"))({theme="cherry", smoothDragging=false})
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/ReliefScript/mogware/refs/heads/main/main.lua"))()
 
-local Window = Library.newWindow({text="Military Roleplay"})
+local Window = Library:Window("Military Roleplay")
 
 -- Tab: Main
 do
-	local Main = Window:addMenu({text="Main"})
+	local Main = Window:Tab("Main")
 
 	-- Section: Anti
 	do
-		local Anti = Main:addSection({text="Anti"})
+		local Anti = Main:Section("Anti")
 
-		Anti:addToggle({text="AntiCuff"}):bindToEvent("onToggle", function(Toggle)
+		Anti:Toggle("AntiCuff", function(Toggle)
 			if Toggle then
 				C1 = LocalPlayer:GetAttributeChangedSignal("Cuffed"):Connect(function()
 					local IsCuffed = LocalPlayer:GetAttribute("Cuffed")
@@ -54,6 +56,45 @@ do
 				C1 = nil
 				C2 = nil
 			end
+		end)
+	end
+
+	-- Section: Spoof
+	do
+		local Spoof = Main:Section("Spoof")
+
+		local Script = PlayerScripts["[ CLIENT ] Engine"]["Game Structure"]["[ OverHead ]"]
+		local Overhead = ReplicatedStorage.GameEvents.OverheadEvent
+		
+		local IsAfk = false
+		local Device = "Mobile"
+
+		local function ToggleAfk(bool)
+			Overhead:FireServer("AFK", bool and "UnFocused" or "Focused")
+		end
+
+		Spoof:Toggle("Enabled", function(Toggle)
+			Script.Disabled = Toggle
+			ToggleAfk(IsAfk)
+		end)
+
+		Spoof:Toggle("AFK", function(Toggle)
+			if Script.Disabled then
+				IsAfk = Toggle
+				ToggleAfk(IsAfk)
+			end
+		end)
+
+		Spoof:Button("Mobile", function()
+			Overhead:FireServer("Device", "Mobile")
+		end)
+
+		Spoof:Button("PC", function()
+			Overhead:FireServer("Device", "PC")
+		end)
+
+		Spoof:Button("Gamepad", function()
+			Overhead:FireServer("Device", "Gamepad")
 		end)
 	end
 end
