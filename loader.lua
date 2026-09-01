6b761cf5883a9f586f529f64b7ce362eb9921f26
d-upre <140529270+d-upre@users.noreply.github.com>
2026-08-31 20:27:05 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -7,7 +7,7 @@ local Id = game.PlaceId
 local Found = Games[Id]
 if Found then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/d-upre/rks-hub/refs/heads/main/games/"..Found..".lua"))()
-else:
+else
     local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/d-upre/rks-hub/refs/heads/main/assets/ui.lua"))({theme="cherry", smoothDragging=false})
     Library.notify({
         title = "Place id: '" .. Id .. "' isn't in pea hub!",
